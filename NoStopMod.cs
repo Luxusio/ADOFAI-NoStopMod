b98ecb1c3b4dfb2de395756a8abad8ac7dcea40f
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-06-09 19:02:36 +0900

    Refactor: move classes, simplify onToggle method

--- NoStopMod.cs
+++ NoStopMod.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
 using System.Reflection;
 using HarmonyLib;
-using UnityEngine;
 using UnityModManagerNet;
-using UnityEngine.Scripting;
+using NoStopMod.GarbageCollection;
+using System.Collections.Generic;
+using NoStopMod.AsyncInput;
 
 namespace NoStopMod
 {
@@ -16,84 +13,45 @@ namespace NoStopMod
 
         public static UnityModManager.ModEntry mod;
         public static Harmony harmony;
+        public static GCManager gcManager;
+        public static AsyncInputManager asyncInputManager;
+        public static List<Action<bool>> onToggleListeners;
 
-        public static bool currentEnabled = false;
-        public static bool ready = false;
-        public static bool gcEnabled = true;
+        //public static bool isEnabled = false;
+        //public static bool ready = false;
 
         public static bool Load(UnityModManager.ModEntry modEntry)
         {
             modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(NoStopMod.OnToggle);
+            NoStopMod.harmony = new Harmony(modEntry.Info.Id);
             NoStopMod.mod = modEntry;
+            NoStopMod.onToggleListeners = new List<Action<bool>>();
+
+            gcManager = new GCManager();
+            asyncInputManager = new AsyncInputManager();
+
             return true;
         }
 
         public static bool OnToggle(UnityModManager.ModEntry modEntry, bool enabled)
         {
             NoStopMod.mod = modEntry;
-            
-            if (NoStopMod.currentEnabled != enabled)
+
+            if (enabled)
             {
-                if (enabled)
-                {
-                    NoStopMod.ready = true;
-                    NoStopMod.harmony = new Harmony(modEntry.Info.Id);
-                    NoStopMod.harmony.PatchAll(Assembly.GetExecutingAssembly());
-                    
-                }
-                else
-                {
-                    NoStopMod.ready = false;
-                    NoStopMod.harmony.UnpatchAll(NoStopMod.harmony.Id);
-                    
-                }
-                NoStopMod.currentEnabled = enabled;
+                NoStopMod.harmony.PatchAll(Assembly.GetExecutingAssembly());
             }
-            NoStopMod.gcEnabled = true;
+            else
+            {
+                NoStopMod.harmony.UnpatchAll(NoStopMod.harmony.Id);
+            }
+
+            foreach (Action<bool> listener in onToggleListeners)
+            {
+                listener.Invoke(enabled);
+            }
+
             return true;
-        }
-
-        public static bool GetDisableAutoSave()
-        {
-            return NoStopMod.ready && !NoStopMod.gcEnabled;
-        }
-
-        public static void DisableGC()
-        {
-            if (NoStopMod.ready)
-            {
-                try
-                {
-                    gcEnabled = false;
-                    //NoStopMod.mod.Logger.Log("disablegc");
-                    GarbageCollector.GCMode = GarbageCollector.Mode.Disabled;
-                    System.Runtime.GCSettings.LatencyMode = System.Runtime.GCLatencyMode.SustainedLowLatency;
-                }
-                catch (NotImplementedException e)
-                {
-                    NoStopMod.mod.Logger.Log("Exception occur");
-                    NoStopMod.mod.Logger.Error(e.ToString());
-                }
-            }
-        }
-
-        public static void EnableGC()
-        {
-            if (NoStopMod.ready)
-            {
-                try
-                {
-                    gcEnabled = true;
-                    //NoStopMod.mod.Logger.Log("enablegc");
-                    GarbageCollector.GCMode = GarbageCollector.Mode.Enabled;
-                    System.Runtime.GCSettings.LatencyMode = System.Runtime.GCLatencyMode.Interactive;
-                    GC.Collect();
-                } catch (NotImplementedException e)
-                {
-                    NoStopMod.mod.Logger.Log("Exception occur");
-                    NoStopMod.mod.Logger.Error(e.ToString());
-                }
-            }
         }
 
     }
