aae07d2fe1a18aea7e9a0e0b2511710df792f9fb
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-06-11 09:22:48 +0900

    Fix: Fix build errors

--- AsyncInput/AsyncInputManager.cs
+++ AsyncInput/AsyncInputManager.cs
@@ -1,6 +1,4 @@
-﻿using DG.Tweening;
-using HarmonyLib;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
--- AsyncInput/AsyncInputPatches.cs
+++ AsyncInput/AsyncInputPatches.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using DG.Tweening;
+using HarmonyLib;
 using System;
 using UnityEngine;
 
