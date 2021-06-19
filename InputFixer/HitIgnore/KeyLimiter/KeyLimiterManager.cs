9cbc7541fd20d56714be5c21398eab45a00df2ef
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-06-19 15:41:17 +0900

    Feat: Create JSONHelper, KeyLimiter

---
+++ InputFixer/HitIgnore/KeyLimiter/KeyLimiterManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace NoStopMod.InputFixer.HitIgnore.KeyLimiter
+{
+    class KeyLimiterManager
+    {
+
+        public static KeyLimiterSettings settings;
+
+        public static void Init()
+        {
+            NoStopMod.onGUIListeners.Add(OnGUI);
+
+            settings = new KeyLimiterSettings();
+            Settings.settings.Add(settings);
+            
+        }
+
+        private static void OnGUI(UnityModManager.ModEntry entry)
+        {
+
+        }
+
+    }
+}
---
+++ InputFixer/HitIgnore/KeyLimiter/KeyLimiterSettings.cs
@@ -0,0 +1,38 @@
+﻿using NoStopMod.Helper;
+using NoStopMod.Helper.Abstraction;
+using SimpleJSON;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NoStopMod.InputFixer.HitIgnore.KeyLimiter
+{
+    class KeyLimiterSettings : SettingsBase
+    {
+
+        public bool enable;
+        public List<KeyCode> limitKeys;
+        
+        public void Load(ref JSONNode json)
+        {
+            JSONNode node = json["KeyLimiter"];
+
+            enable = node["enable"].AsBool;
+            limitKeys = JSONHelper.ReadArray(ref node, "limitKeys", (arrayNode) => { return (KeyCode) arrayNode.AsInt; });
+            
+            throw new NotImplementedException();
+        }
+
+        public void Save(ref JSONNode json)
+        {
+            JSONNode node = JSONHelper.CreateEmptyNode();
+            node["enable"].AsBool = enable;
+            node["limitKeys"] = JSONHelper.WriteArray(limitKeys, (element) => { return (int) element; });
+
+            json["KeyLimiter"] = node;
+        }
+    }
+}
