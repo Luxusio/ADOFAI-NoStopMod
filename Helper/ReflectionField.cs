ac970a9f7b78836ff498f7149758cabface1a35e
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-06-24 13:04:07 +0900

    Feat: Make ReflectionField for easy, multiple field names reflection

---
+++ Helper/ReflectionField.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace NoStopMod.Helper
+{
+    class ReflectionField
+    {
+        private static FieldInfo fi;
+
+        private string[] fieldNames;
+
+        public ReflectionField(params string[] fieldNames)
+        {
+            this.fieldNames = fieldNames;
+        }
+
+        public FieldInfo getFieldInfo(Type type)
+        {
+            if (fi == null)
+            {
+                for (int i=0;i < fieldNames.Count();i++)
+                {
+                    fi = type.GetField(fieldNames[i], BindingFlags.NonPublic | BindingFlags.Instance);
+                    if (fi != null) break;
+                }
+            }
+            return fi;
+        }
+
+    }
+}
