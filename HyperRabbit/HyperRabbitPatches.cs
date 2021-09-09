c706216921553388117fd0cfbb224b2821461caf
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-09-09 17:27:44 +0900

    Chore(HyperRabbit): Optimize mechanism

--- HyperRabbit/HyperRabbitPatches.cs
+++ HyperRabbit/HyperRabbitPatches.cs
@@ -4,45 +4,22 @@ namespace NoStopMod.HyperRabbit
 {
     class HyperRabbitPatches
     {
-        
-        [HarmonyPatch(typeof(scrPlanet), "SwitchChosen")]
-        private static class PlanetSwitchChosenPatch
-        {
-            public static void Prefix(scrPlanet __instance)
-            {
-                if (RDC.auto && scrController.isGameWorld)
-                {
-                    __instance.angle = __instance.targetExitAngle;
-                }
-            }
-        }
 
         [HarmonyPatch(typeof(scrController), "PlayerControl_Update")]
         private static class scrController_PlayerControl_Update_Patch
         {
             public static void Postfix(scrController __instance)
             {
-                if (!RDC.auto || !scrController.isGameWorld)
+                if (RDC.auto && scrController.isGameWorld)
                 {
-                    return;
+                    int num = 1000;
+                    while (num > 0 && __instance.chosenplanet.AutoShouldHitNow())
+                    {
+                        __instance.keyBufferCount = 0;
+                        __instance.Hit();
+                        num--;
+                    }
                 }
-
-                __instance.chosenplanet.Update_RefreshAngles();
-                while (__instance.chosenplanet.AutoShouldHitNow())
-                {
-                    __instance.keyBufferCount = 0;
-                    __instance.Hit();
-                    __instance.chosenplanet.Update_RefreshAngles();
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(scrController), "FailAction")]
-        private static class ControllerFailActionPatch
-        {
-            public static bool Prefix()
-            {
-                return !RDC.auto;
             }
         }
 
