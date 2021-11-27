363cf79b725019f2e6fdb18972176ffaf0f937d9
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-11-28 03:22:47 +0900

    Build: Fix build command

--- make_debug.bat
+++ make_debug.bat
@@ -7,6 +7,7 @@ copy ..\bin\Debug\NoStopMod.dll NoStopMod
 copy ..\Info.json NoStopMod
 
 tar -a -c -f NoStopMod-%1.zip NoStopMod
+del /f /q ../NoStopMod-%1.zip
 move NoStopMod-%1.zip ..
 cd ..
 rmdir /s /q tmp
